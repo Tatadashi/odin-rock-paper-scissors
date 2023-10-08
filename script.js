66d5c2d33fc0b97836de3b82304b676b3ce45b7f
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-10-07 19:22:06 -0700

    Create game function, fix cancel in playround()
    Cancelling out of input will not break program now. Game function plays 5 rounds and keeps score, outputting victor

--- script.js
+++ script.js
@@ -21,26 +21,71 @@ function getComputerChoice () {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection !== null) {
+        playerSelection = playerSelection.toLowerCase();
+    }
     computerSelection = computerSelection.toLowerCase();
 
     let outcome = "nothing";
 
     if (playerSelection === computerSelection) {
-        outcome = "Tie";
+        outcome = "You Tie!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        outcome = "Computer wins round";
+        outcome = "You Lose! Paper beats Rock";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        outcome = "Player wins round";
+        outcome = "You Win! Rock beats Scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        outcome = "Player wins round";
+        outcome = "You Win! Paper beats Rock";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        outcome = "Computer wins round";
+        outcome = "You Lose! Scissors beats Paper";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        outcome = "Computer wins round";
+        outcome = "You Lose! Rock beats Scissors";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        outcome = "Player wins round";
+        outcome = "You Win! Scissors beats Paper";
+    } else {
+        outcome = "nothing"
     }
 
     return outcome;
 }
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper, Scissors? (Type 1 one of these)");
+        const computerSelection = getComputerChoice();
+
+        const outcome = playRound(playerSelection, computerSelection);
+        console.log(outcome);
+
+        const result = outcome.slice(0, 8);
+        
+        switch (result) {
+            case "You Win!":
+                playerScore++;
+                break;
+            case "You Lose":
+                computerScore++;
+                break;
+            case "You Tie!":
+                break;
+            default:
+                break;
+        }
+    }
+
+    let victor = "no one";
+    if (playerScore > computerScore) {
+        victor = `Player wins game ${playerScore} to ${computerScore}!`;
+    } else if (playerScore < computerScore) {
+        victor = `Computer wins game ${computerScore} to ${playerScore}!`
+    } else {
+        victor = `Tie with ${playerScore}!`
+    }
+
+    console.log(victor);
+}
+
+game();
